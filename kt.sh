c08c96314ee8bfb459bc034743fb95ce0032f9e2
SaturnXIII <110695125+SaturnXIII@users.noreply.github.com>
2023-01-29 18:57:46 +0100

    1.3

--- kt.sh
+++ kt.sh
@@ -4,19 +4,50 @@
 sudo apt-get update
 
 # Installation des outils de pentest
+echo "Installation de nmap"
 sudo apt-get install nmap
+
+echo "Installation d'aircrack-ng"
 sudo apt-get install aircrack-ng
+
+echo "Installation de sqlmap"
 sudo apt-get install sqlmap
+
+echo "Installation de metasploit-framework"
 sudo apt-get install metasploit-framework
+
+echo "Installation de netcat"
 sudo apt-get install netcat
+
+echo "Installation de net-tools"
 sudo apt-get install net-tools
+
+echo "Installation de wireshark"
 sudo apt-get install wireshark
+
+echo "Installation de john"
 sudo apt-get install john
+
+echo "Installation de hashcat"
 sudo apt-get install hashcat
+
+echo "Installation de burpsuite"
 sudo apt-get install burpsuite
 
 # Installation des outils de sécurité
+echo "Installation de openvpn"
 sudo apt-get install openvpn
+
+echo "Installation de openssh-server"
 sudo apt-get install openssh-server
+
+echo "Installation de fail2ban"
 sudo apt-get install fail2ban
+
+echo "Installation de ufw"
 sudo apt-get install ufw
+
+echo "Installation Metasploit"
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall &&
+chmod 755 msfinstall &&
+./msfinstall
