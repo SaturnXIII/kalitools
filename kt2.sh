3a95a5856b919b530e17e326937487ec0b49b46f
SaturnXIII <110695125+SaturnXIII@users.noreply.github.com>
2023-02-01 22:43:18 +0100

    Update kt2.sh

--- kt2.sh
+++ kt2.sh
@@ -9,60 +9,60 @@ sudo apt-get update
 
 # Installation des outils de pentest
 echo "Installation de nmap" | lolcat
-sudo apt-get install nmap 
+sudo apt-get install nmap -y
 figlet "nmap" | lolcat
 
 echo "Installation d'aircrack-ng" | lolcat
-sudo apt-get install aircrack-ng
+sudo apt-get install aircrack-ng -y
 figlet "aircrack-ng" | lolcat
 
 echo "Installation de sqlmap" | lolcat
-sudo apt-get install sqlmap
+sudo apt-get install sqlmap -y
 figlet "sqlmap" | lolcat
 
 echo "Installation de metasploit-framework" | lolcat
-sudo apt-get install metasploit-framework
+sudo apt-get install metasploit-framework -y
 figlet "metasploit-framework" | lolcat
 
 echo "Installation de netcat" | lolcat
-sudo apt-get install netcat
+sudo apt-get install netcat -y
 figlet "netcat" | lolcat
 
 echo "Installation de net-tools" | lolcat
-sudo apt-get install net-tools
+sudo apt-get install net-tools -y
 figlet "net-tools" | lolcat
 
 echo "Installation de wireshark" | lolcat
-sudo apt-get install wireshark
+sudo apt-get install wireshark -y
 figlet "wireshark" | lolcat
 
 echo "Installation de john" | lolcat
-sudo apt-get install john
+sudo apt-get install john -y
 figlet "john" | lolcat
 
 echo "Installation de hashcat" | lolcat
-sudo apt-get install hashcat
+sudo apt-get install hashcat -y
 figlet "hashcat" | lolcat
 
 echo "Installation de burpsuite" | lolcat
-sudo apt-get install burpsuite
+sudo apt-get install burpsuite -y
 figlet "burpsuite" | lolcat
 
 # Installation des outils de sécurité
 echo "Installation de openvpn" | lolcat
-sudo apt-get install openvpn
+sudo apt-get install openvpn -y
 figlet "openvpn" | lolcat
 
 echo "Installation de openssh-server" | lolcat
-sudo apt-get install openssh-server
+sudo apt-get install openssh-server -y
 figlet "openssh-server" | lolcat
 
 echo "Installation de fail2ban" | lolcat
-sudo apt-get install fail2ban
+sudo apt-get install fail2ban -y
 figlet "fail2ban" | lolcat
 
 echo "Installation de ufw" | lolcat
-sudo apt-get install ufw
+sudo apt-get install ufw -y
 figlet "ufw" | lolcat
 
 #!/bin/bash
